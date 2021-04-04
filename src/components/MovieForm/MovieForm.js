3ba995933763f4bf27276929bbba630cf60015de
Jessica Paul <paul.jessica95@gmail.com>
2021-04-04 19:22:07 -0400

    connect to API

--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -1,17 +1,24 @@
 import { useContext, useState } from 'react';
 import { WatchedContext } from '../../contexts/WatchedContext';
+import { searchMovie } from '../../services/omdb-api';
 
 const MovieForm = () => {
     const { dispatch } = useContext(WatchedContext);
     const [ title, setTitle ] = useState('');
+
+    async function getMovieData(searchTitle) {
+        const data = await searchMovie(searchTitle);
+        console.log('data: ', data);
+    }
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
         dispatch({type: 'ADD_MOVIE', movie: {
             title
         }});
-        console.log(title);
-        setTitle('')
+        getMovieData(title);
+        console.log('title: ', title);
+        setTitle('');
     }
     return (
         <form onSubmit={handleSubmit}>
